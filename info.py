bec0002458422ed41087ff7f9f414e595fead898
MoviesbizzYT <89892012+MoviesbizzYT@users.noreply.github.com>
2025-04-05 21:53:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesbizzchat')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesbizzchat1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/moviesbizzmalayalam_hd')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'moviesbizzbotz') # Support Chat Link Without https:// or @
