3a966800510b3024906e88e1778b055509f33678
CRIZZBIZZ <86725697+CRIZZBIZZ@users.noreply.github.com>
2025-04-14 17:10:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1847,9 +1847,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             )
             button = [[
                 InlineKeyboardButton("🚀 Fast Download 🚀", url=download),
-                InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)
             ],[
-                InlineKeyboardButton("• ᴡᴀᴛᴄʜ ɪɴ ᴡᴇʙ ᴀᴘᴘ •", web_app=WebAppInfo(url=stream))
+                InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)
             ]]
             await query.message.reply_text(
                 text="•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ☠︎⚔",
